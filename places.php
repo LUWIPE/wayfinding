efe729a04081a3cb4c49fc30814ffb70e50a790c
Lukas <lukaswp2710@gmail.com>
2024-04-09 11:42:49 +0200

    Tilføjet places, category og subCategory fra database og styled dem

--- places.php
+++ places.php
@@ -18,30 +18,113 @@ require "settings/init.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body
-<div class="row g-2">
-    <?php
-    $produkter = $db->sql("SELECT * FROM places inner join FROM category");
-    foreach($produkter as $places) {
-        ?>
-        <div class="col-12 col-md-6">
-            <div class="card w-100">
-                <div class="card-header">
-                    <?php
-                    echo $places->placeCaregory;
-                    ?>
+<body>
+<div class="container">
+    <div class="row">
+        <div></div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row justify-content-around col-12 mt-3">
+        <div class="row col-4 card justify-content-center h-100 bg-primary pb-5">
+            <div class="col-11 mt-3 border-bottom border-senary border-2">
+                <strong class="fs-4 text-gold">
+                    Kategorier:
+                </strong>
+            </div>
+            <?php
+            $categories = $db->sql("SELECT * FROM category");
+            foreach ($categories as $category) {
+                ?>
+                <div class="col-11">
+                    <div class=" w-100">
+                        <div class="fs-5 m-2">
+                            <?php
+                            echo $category->cateName;
+                            ?>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-header">
-                    <?php
-                    echo $places->placeName;
-                    ?>
+                <?php
+            }
+            ?>
+            <div class="col-11 mt-4 border-bottom border-senary border-2">
+                <strong class="fs-4 text-gold">
+                    Subkategorier:
+                </strong>
+            </div>
+            <?php
+            $subCategories = $db->sql("SELECT * FROM subCategory");
+            foreach ($subCategories as $subCategory) {
+                ?>
+                <div class="col-11">
+                    <div class=" w-100">
+                        <div class="fs-5 m-2">
+                            <?php
+                            echo $subCategory->subCateName;
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+        <div class="row col-8 g-2 justify-content-center m-0">
+            <div class="d-flex align-content-start m-0">
+                <div class="input-group col-8">
+                    <input type="text" class="form-control text-secondary bg-primary border-2 border-gold fs-4" id="placeSearch" placeholder="Søg her..." aria-label="place" value="">
+                    <button type="submit" class="btn btn-gold text-quinary fs-4" id="searchBtn">Søg</button>
                 </div>
             </div>
+            <div class="row justify-content-around mt-3">
+                <?php
+                $places = $db->sql("SELECT * FROM places INNER JOIN category ON placeCategory = cateId");
+                foreach ($places as $place) {
+                    ?>
+                    <div class="col-5 mb-4 p-1 card bg-primary d-flex align-content-center pt-5 pb-5">
+                        <div class="w-100 text-center ">
+                            <div class="fs-6 text-gold">
+                                <?php
+                                echo $place->cateName;
+                                ?>
+                            </div>
+                            <div class="fs-5 mt-2 mb-2">
+                                <strong>
+                                    <?php
+                                    echo $place->placeName; //sorter alfabetisk//
+                                    ?></strong>
+                            </div>
+                            <div class="fs-6">
+                                <?php
+                                echo $place->streetName . ' ' . $place->streetNumber;
+                                ?>
+                            </div>
+                            <div class="fs-6 d-none">
+                                <?php
+                                echo $place->postalCode . ' ' . $place->cityName;
+                                ?>
+                            </div>
+                            <div class="fs-6 d-none">
+                                <?php
+                                echo $place->phoneNumber; //indsæt if statement//
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
         </div>
-        <?php
-    }
-    ?>
+    </div>
 </div>
+
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
