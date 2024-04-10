d430be5e5379c126ed884c2c158647a883ea82dd
Lukas <lukaswp2710@gmail.com>
2024-04-10 12:34:25 +0200

    Tilføjet filterfunktion

--- places.php
+++ places.php
@@ -21,36 +21,44 @@ require "settings/init.php";
 <body>
 <div class="container mt-3">
     <div class="row justify-content-between">
-        <div class="col-2"><a href="index.php"><img class="img-fluid" src="images/voresnyk-logo-gold.png" alt=""></a></div>
+        <div class="col-2"><a href="index.php"><img class="img-fluid" src="images/voresnyk-logo-gold.png" alt=""></a>
+        </div>
         <div class="d-flex col-4 justify-content-between align-items-center">
-            <div class="flag ms-3"><img class="flag rounded-circle border border-5 border-gold" src="images/denmark.png" alt=""></div>
-            <div class="flag"><img class="flag rounded-circle border border-2 border-secondary" src="images/britain.png" alt=""></div>
-            <div class="flag me-3"><img class="flag rounded-circle border border-2 border-secondary" src="images/germany.png" alt=""></div>
+            <div class="flag ms-3"><img class="flag rounded-circle border border-5 border-gold" src="images/denmark.png"
+                                        alt=""></div>
+            <div class="flag"><img class="flag rounded-circle border border-2 border-secondary" src="images/britain.png"
+                                   alt=""></div>
+            <div class="flag me-3"><img class="flag rounded-circle border border-2 border-secondary"
+                                        src="images/germany.png" alt=""></div>
         </div>
     </div>
 </div>
 <div class="container">
     <div class="row justify-content-around col-12 mt-3">
-        <div class="row col-4 card justify-content-center h-100 bg-primary pb-5">
+        <div class="row col-4 card justify-content-center align-items-start h-100 bg-primary pb-4">
             <div class="col-11 mt-3 border-bottom border-senary border-2">
                 <strong class="fs-4 text-gold">
                     Kategorier:
                 </strong>
             </div>
-            <?php
-            $categories = $db->sql("SELECT * FROM category");
-            foreach ($categories as $category) {
+            <div class="col-11">
+                <?php
+                $categories = $db->sql("SELECT * FROM category");
+                foreach ($categories
+
+                as $category) {
                 ?>
-                <div class="col-11">
-                    <div class=" w-100">
-                        <div class="fs-5 m-2">
+
+                <div class=" w-100">
+                    <div class="fs-5 m-2 text-secondary">
+                        <a class="text-secondary" href="places.php?cateId=<?php echo $category->cateId ?>">
                             <?php
                             echo $category->cateName;
-                            ?>
-                        </div>
+                            ?></a>
                     </div>
                 </div>
-                <?php
+            </div>
+            <?php
             }
             ?>
             <div class="col-11 mt-4 border-bottom border-senary border-2">
@@ -58,16 +66,18 @@ require "settings/init.php";
                     Subkategorier:
                 </strong>
             </div>
+            <div class="col-11">
             <?php
             $subCategories = $db->sql("SELECT * FROM subCategory");
             foreach ($subCategories as $subCategory) {
                 ?>
-                <div class="col-11">
+
                     <div class=" w-100">
-                        <div class="fs-5 m-2">
-                            <?php
-                            echo $subCategory->subCateName;
-                            ?>
+                        <div class="fs-5 m-2 text-secondary">
+                            <a class="text-secondary" href="places.php?subCateId=<?php echo $subCategory->subCateId ?>">
+                                <?php
+                                echo $subCategory->subCateName;
+                                ?></a>
                         </div>
                     </div>
                 </div>
@@ -75,19 +85,34 @@ require "settings/init.php";
             }
             ?>
         </div>
-        <div class="row col-8 g-2 justify-content-center m-0">
+        <div class="row col-8 justify-content-center m-0">
             <div class="d-flex align-content-start m-0">
-                <div class="input-group col-8">
-                    <input type="text" class="form-control text-secondary bg-primary border-2 border-gold fs-4" id="placeSearch" placeholder="Søg her..." aria-label="place" value="">
+                <div class="input-group col-12">
+                    <input type="text" class="form-control text-secondary bg-primary border-2 border-gold fs-4"
+                           id="placeSearch" placeholder="Søg her..." aria-label="place" value="">
                     <button type="submit" class="btn btn-gold text-quinary fs-4" id="searchBtn">Søg</button>
                 </div>
             </div>
-            <div class="row justify-content-around mt-3">
+            <div class="row justify-content-between mt-3">
                 <?php
-                $places = $db->sql("SELECT * FROM places INNER JOIN category ON placeCategory = cateId");
+                $sqladd = "";
+                $bind = [];
+
+                if (!empty($_GET["cateId"])) {
+                    $sqladd = " AND cateId = :cateId";
+                    $bind[":cateId"] = $_GET["cateId"];
+                }
+                if (!empty($_GET["subCateId"])) {
+                    $sqladd = " AND subCateId = :subCateId";
+                    $bind[":subCateId"] = $_GET["subCateId"];
+                }
+
+                $sqlPlaces = "SELECT * FROM places INNER JOIN category ON placeCategory = cateId WHERE 1=1" . $sqladd;
+                $places = $db->sql($sqlPlaces, $bind);
                 foreach ($places as $place) {
                     ?>
-                    <div class="col-5 mb-4 p-1 card bg-primary d-flex align-content-center pt-5 pb-5">
+                    <div class="col-5 m-4 p-1 card bg-primary d-flex align-content-center pt-5 pb-5"
+                         id="<?php echo($place->placeId) ?>">
                         <div class="w-100 text-center ">
                             <div class="fs-6 text-gold">
                                 <?php
@@ -127,5 +152,8 @@ require "settings/init.php";
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+
+</script>
 </body>
 </html>
