bc0ca65ceee838d68bedc7b2f25c83ddb6a46f5e
Lukas <lukaswp2710@gmail.com>
2024-04-12 10:57:38 +0200

    Tilføjet maps til index

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-require "settings/init.php";
-?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
@@ -13,13 +10,30 @@ require "settings/init.php";
     <meta name="copyright" content="Information om copyright">
 
     <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.typekit.net/lla8zep.css">
+    <script src="https://kit.fontawesome.com/b608edf7c6.js" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+<body>
 
-<body
+<div id="googleMap" style="width:100%;height:90vh;"></div>
 
 
+<script>
+    function myMap() {
+        var mapProp= {
+            center:new google.maps.LatLng(54.7662677,11.8716984),
+            zoom:20,
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+
+
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUwmkWzSdJsxdloWRUlF2ZLXj-3Z6WLVU&callback=myMap"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
