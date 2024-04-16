f313c9e14c44e99b0aa733c6a4aeb461ba41a1ca
Lukas <lukaswp2710@gmail.com>
2024-04-16 15:11:24 +0200

    Rettelser i opsætningen så det står magen til places

--- search.php
+++ search.php
@@ -36,116 +36,127 @@ require "settings/init.php";
     </div>
 </div>
 <div class="container">
-    <div class="row justify-content-around col-12 mt-3">
-        <div class="row col-4 card justify-content-center align-items-start bg-primary pb-4 h-100">
-            <div class="col-11 mt-3 border-bottom border-senary border-2">
-                <strong class="fs-4 text-gold">
-                    Kategorier:
-                </strong>
-            </div>
-            <?php
-            $categories = $db->sql("SELECT * FROM category");
-            foreach ($categories as $category) {
-                ?>
-                <div class="col-11">
-                    <div class="w-100">
-                        <div class="fs-5 m-2 text-secondary" id="<?php echo($category->cateId) ?>">
-                            <a class="text-secondary" href="places.php?cateId=<?php echo $category->cateId ?>">
-                                <?php
-                                echo $category->cateName;
-                                ?></a>
-                        </div>
-                    </div>
+    <div class="row justify-content-around mt-3">
+        <div class="col-4 card justify-content-center align-items-start bg-primary pb-4 h-100">
+            <div class="row">
+                <div class="col-11 mt-3 border-bottom border-senary border-2">
+                    <strong class="fs-4 text-gold">
+                        Filtre:
+                    </strong>
                 </div>
                 <?php
-            }
-            ?>
-            <div class="col-11 mt-4 border-bottom border-senary border-2">
-                <strong class="fs-4 text-gold">
-                    Subkategorier:
-                </strong>
-            </div>
-            <?php
-            $subCategories = $db->sql("SELECT * FROM subCategory");
-            foreach ($subCategories as $subCategory) {
-                ?>
-                <div class="col-11">
-                    <div class="w-100">
-                        <div class="fs-5 m-2 text-secondary" id="<?php echo($subCategory->subCateId) ?>">
-                            <a class="text-secondary" href="places.php?subCateId=<?php echo $subCategory->subCateId ?>">
-                                <?php
-                                echo $subCategory->subCateName;
-                                ?></a>
+                $categories = $db->sql("SELECT * FROM category");
+                foreach ($categories as $category) {
+                    ?>
+                    <div class="col-11">
+                        <div class="w-100">
+                            <div class="fs-5 m-2 text-secondary" id="<?php echo($category->cateId) ?>">
+                                <a id="cateBtn" class="text-secondary"
+                                   href="places.php?cateId=<?php echo $category->cateId;
+                                   echo (!empty($_GET["subCateId"])) ? '&subCateId=' . $_GET["subCateId"] : ""; ?>">
+                                    <?php
+                                    echo $category->cateName;
+                                    ?></a>
+                            </div>
                         </div>
                     </div>
+                    <?php
+                }
+                ?>
+                <div class="col-11 mt-4 border-bottom border-senary border-2">
+                    <strong class="fs-4 text-gold">
+                        Kategorier:
+                    </strong>
                 </div>
                 <?php
-            }
-            ?>
+                $subCategories = $db->sql("SELECT * FROM subCategory WHERE subCateId > 1");
+                foreach ($subCategories as $subCategory) {
+                    ?>
+                    <div class="col-11">
+                        <div class="w-100">
+                            <div class="fs-5 m-2 text-secondary" id="<?php echo($subCategory->subCateId) ?>">
+                                <a id="subCateBtn" class="text-secondary"
+                                   href="places.php?<?php echo (!empty($_GET["cateId"])) ? '&cateId=' . $_GET["cateId"] : ""; ?>&subCateId=<?php echo $subCategory->subCateId ?>">
+                                    <?php
+                                    echo $subCategory->subCateName;
+                                    ?></a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
         </div>
-        <div class="row col-8 justify-content-center m-0 p-0">
-            <form class="p-0" action="search.php" method="post">
-                <div class="d-flex align-content-start m-0 p-0 mb-3 ms-5">
-                    <div class=" input-group">
-                        <input class="form-control text-secondary bg-primary border-2 border-gold fs-4 "
-                               aria-label="place" type="text" name="search" placeholder="Søg her...">
-                        <input class="btn btn-gold text-quinary fs-4" id="searchBtn" type="submit" value="Søg"
-                               name="submit"">
+        <div class="col-8 justify-content-center m-0 p-0">
+            <div class="row justify-content-start p-0">
+                <form class="p-0" action="search.php" method="post">
+                    <div class="d-flex align-content-start m-0 p-0 mb-3 ms-5">
+                        <div class=" input-group">
+                            <input class="form-control text-secondary bg-primary border-2 border-gold fs-4 "
+                                   aria-label="place" type="text" name="search" placeholder="Søg her...">
+                            <input class="btn btn-gold text-quinary fs-4" id="searchBtn" type="submit" value="Søg"
+                                   name="submit"">
+                        </div>
                     </div>
-                </div>
-            </form>
-            <div class="row justify-content-between mt-3" id="placesSearch">
+                </form>
+            </div>
+            <div class="row justify-content-between mt-3 p-0" id="placesSearch">
 
                 <?php
 
                 $search = $_POST['search'];
 
-                $sql = "SELECT * FROM places INNER JOIN category ON cateId WHERE placeName LIKE CONCAT('%', :search, '%')";
+                $sql = "SELECT * FROM places INNER JOIN category ON cateId = placeCategory WHERE placeName LIKE CONCAT('%', :search, '%')";
                 $bind = [":search" => $search];
                 $result = $db->sql($sql, $bind);
 
                 if (!empty($result)) {
-                    foreach ($result as $place) {
-                        ?>
-                        <div class="col-5 mb-5 ms-5 p-1 card bg-primary d-flex align-content-center pt-5 pb-5"
-                             id="<?php echo($place->placeId) ?>">
-                            <a href="destination.php?placeId=<?php echo $place->placeId ?>" id="destination">
-                                <div class="w-100 text-center ">
-                                    <div class="fs-6 text-gold">
-                                        <?php
-                                        echo $place->cateName;
-                                        ?>
-                                    </div>
-                                    <div class="fs-5 mt-2 mb-2 text-secondary">
-                                        <strong>
-                                            <?php
-                                            echo $place->placeName; //sorter alfabetisk//
-                                            ?></strong>
-                                    </div>
-                                    <div class="fs-6 text-secondary">
-                                        <?php
-                                        echo $place->streetName . ' ' . $place->streetNumber;
-                                        ?>
-                                    </div>
-                                    <div class="fs-6 d-none text-secondary">
-                                        <?php
-                                        echo $place->postalCode . ' ' . $place->cityName;
-                                        ?>
-                                    </div>
-                                    <div class="fs-6 d-none text-secondary">
-                                        <?php
-                                        echo $place->phoneNumber;
-                                        ?>
-                                    </div>
+                foreach ($result
+
+                         as $place) {
+                    ?>
+                    <div class="col-5 mb-5 ms-5 p-1 card bg-primary d-flex align-content-center pt-5 pb-5"
+                         id="<?php echo($place->placeId) ?>">
+                        <a href="destination.php?placeId=<?php echo $place->placeId ?>" id="destination">
+                            <div class="w-100 text-center ">
+                                <div class="fs-6 text-gold">
+                                    <?php
+                                    echo $place->cateName;
+                                    ?>
                                 </div>
-                            </a>
-                        </div>
-                        <?php
-                    }
-                } else {
-                    echo '<h1 class="d-flex justify-content-center mt-3">Vi kunne ikke finde noget det matcher din søgning</h1>';
+                                <div class="fs-5 mt-2 mb-2 text-secondary">
+                                    <strong>
+                                        <?php
+                                        echo $place->placeName; //sorter alfabetisk//
+                                        ?></strong>
+                                </div>
+                                <div class="fs-6 text-secondary">
+                                    <?php
+                                    echo $place->streetName . ' ' . $place->streetNumber;
+                                    ?>
+                                </div>
+                                <div class="fs-6 d-none text-secondary">
+                                    <?php
+                                    echo $place->postalCode . ' ' . $place->cityName;
+                                    ?>
+                                </div>
+                                <div class="fs-6 d-none text-secondary">
+                                    <?php
+                                    echo $place->phoneNumber;
+                                    ?>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <?php
                 }
                 ?>
+                    <?php
+                    } else {
+                        echo '<h2 class="text-end">Vi kunne ikke finde noget der matcher din søgning</h2>';
+                    }
+                    ?>
             </div>
         </div>
     </div>
